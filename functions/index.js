4bb9ae4a5524c112ac431219abc58557a90f53bf
P541M <videna.psalmeleazar@gmail.com>
2024-05-21 13:23:25 -0400

    last commit before deploy

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,16 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.scheduledFunction = functions.pubsub
-  .schedule("0 0 * * *")
-  .timeZone("America/New_York")
-  .onRun((context) => {
-    const ref = admin.database().ref("messages");
-    return ref
-      .remove()
-      .then(() => {
-        console.log("Messages cleared at midnight EST");
-      })
-      .catch((error) => {
-        console.error("Error clearing messages:", error);
-      });
-  });
+    .schedule("0 0 * * *")
+    .timeZone("America/New_York")
+    .onRun((context) => {
+      const ref = admin.database().ref("messages");
+      return ref
+          .remove()
+          .then(() => {
+            console.log("Messages cleared at midnight EST");
+          })
+          .catch((error) => {
+            console.error("Error clearing messages:", error);
+          });
+    });
