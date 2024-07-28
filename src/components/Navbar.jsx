28fab09f815f3e4d63d7448f579990500d4f2e2c
P541M <videna.psalmeleazar@gmail.com>
2024-07-28 16:27:03 -0400

    feat: semi finalized navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { HiSun, HiMoon } from "react-icons/hi";
 
 const Navbar = ({ goToSection }) => {
   const [nav, setNav] = useState(false);
-  const [darkMode, setDarkMode] = useState(true);
 
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+  const handleNav = () => setNav(!nav);
 
   useEffect(() => {
     const handleResize = () => {
@@ -22,68 +14,52 @@ const Navbar = ({ goToSection }) => {
     };
 
     window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [darkMode]);
-
   return (
-    <div className="z-50 fixed top-0 left-0 right-0 flex justify-between items-center max-w-auto h-20 px-4 text-l bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text navbar">
-      <a
-        href="#home"
-        className="p-5 lg:ml-0 mx-auto nav-link"
-        onClick={() => goToSection("home")}
-      >
-        <span>Echo</span>
-      </a>
-      <ul className="hidden lg:flex font-semibold items-center">
-        <li className="p-5 text nav-link">
-          <a href="#about" onClick={() => goToSection("about")}>
-            <span>About</span>
-          </a>
-        </li>
-        <li className="p-5 nav-link">
-          <a href="#post-message" onClick={() => goToSection("post-message")}>
-            <span>Post a Message</span>
-          </a>
-        </li>
-        <li className="p-5 nav-link">
-          <a href="#messages" onClick={() => goToSection("messages")}>
-            <span>Message Board</span>
-          </a>
-        </li>
-        <li className="p-5 flex items-center">
-          <button onClick={toggleDarkMode} className="nav-link">
-            {darkMode ? (
-              <HiSun className="text-2xl" />
-            ) : (
-              <HiMoon className="text-2xl" />
-            )}
-          </button>
-        </li>
-      </ul>
-      <div
-        onClick={handleNav}
-        className="block lg:hidden fixed top-4 right-4 z-30"
-      >
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+    <div className="z-[1000] fixed left-0 right-0 h-20 bg-background text-text">
+      <div className="flex justify-between items-center h-full px-8">
+        <div
+          onClick={() => goToSection("home")}
+          className="logo-container nav-link cursor-pointer"
+        >
+          <span className="text-2xl font-bold">Echo</span>
+        </div>
+
+        <ul className="hidden lg:flex font-medium items-center text-lg space-x-8">
+          <li className="nav-link">
+            <a href="#about" onClick={() => goToSection("about")}>
+              About
+            </a>
+          </li>
+          <li className="nav-link">
+            <a href="#messages" onClick={() => goToSection("messages")}>
+              Echoes
+            </a>
+          </li>
+          <li className="nav-link">
+            <a href="#post-message" onClick={() => goToSection("post-message")}>
+              Make an Echo
+            </a>
+          </li>
+        </ul>
+
+        <div
+          onClick={handleNav}
+          className="block lg:hidden fixed top-4 right-4 z-30"
+        >
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
       </div>
+
       <div
-        className={`navbar-small-screen ${
-          nav ? "active" : ""
-        } fixed top-20 left-0 w-full shadow-lg z-20 bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text lg:hidden`}
+        className={`${
+          nav ? "navbar-small-screen active" : "navbar-small-screen"
+        } fixed top-20 left-0 w-full z-20 bg-background text-text lg:hidden`}
       >
         <ul className="font-semibold text-center">
-          <li className="py-5 nav-link">
+          <li className="my-3 nav-link relative">
             <a
               href="#about"
               onClick={() => {
@@ -91,21 +67,10 @@ const Navbar = ({ goToSection }) => {
                 goToSection("about");
               }}
             >
-              <span>About</span>
+              About
             </a>
           </li>
-          <li className="py-5 nav-link">
-            <a
-              href="#post-message"
-              onClick={() => {
-                handleNav();
-                goToSection("post-message");
-              }}
-            >
-              <span>Post a Message</span>
-            </a>
-          </li>
-          <li className="py-5 nav-link">
+          <li className="my-3 nav-link relative">
             <a
               href="#messages"
               onClick={() => {
@@ -113,17 +78,19 @@ const Navbar = ({ goToSection }) => {
                 goToSection("messages");
               }}
             >
-              <span>Message Board</span>
+              Echoes
             </a>
           </li>
-          <li className="py-5 nav-link">
-            <button onClick={toggleDarkMode}>
-              {darkMode ? (
-                <HiSun className="text-2xl" />
-              ) : (
-                <HiMoon className="text-2xl" />
-              )}
-            </button>
+          <li className="my-3 nav-link relative">
+            <a
+              href="#post-message"
+              onClick={() => {
+                handleNav();
+                goToSection("post-message");
+              }}
+            >
+              Make an Echo
+            </a>
           </li>
         </ul>
       </div>
