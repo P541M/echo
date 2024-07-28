af59bf1ccf54579b1c15e7c568c81656c6e69f93
P541M <videna.psalmeleazar@gmail.com>
2024-07-28 15:02:12 -0400

    feat: finalized landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,8 +5,10 @@ const LandingPage = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-background">
       <div className="flex flex-col items-center">
-        <h1 className="text-5xl font-bold text-text">Echo</h1>
-        <p className="text-xl pt-3 text-text">Your Voice, Your Echo</p>
+        <h1 className="text-7xl font-bold text-text">Echo</h1>
+        <p className="text-2xl font-medium mt-2 text-text">
+          Your Voice, Your Echo
+        </p>
       </div>
     </div>
   );
