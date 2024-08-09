06c2c118f78fcc1178c5535c2d7fb71b33b762ac
P541M <videna.psalmeleazar@gmail.com>
2024-08-08 20:09:16 -0400

    feat: finalized styling of boxes

--- src/components/MessagesSection.jsx
+++ src/components/MessagesSection.jsx
@@ -17,25 +17,26 @@ const MessagesSection = ({ messages, likeMessage, userId }) => {
               .map((message) => (
                 <div
                   key={message.id}
-                  className="relative flex h-64 flex-col justify-between rounded-2xl bg-secondary p-6 shadow-lg transition-transform duration-200 ease-in-out hover:scale-105"
+                  className="relative flex flex-col rounded-2xl bg-secondary p-6 shadow-lg transition-transform duration-200 ease-in-out hover:scale-105"
                 >
-                  <p className="mb-4 text-lg">{message.text}</p>
-                  <div className="absolute bottom-4 left-4 flex items-center">
-                    <button
-                      onClick={() => likeMessage(message.id)}
-                      className="text-accent transition-transform duration-200 ease-in-out hover:scale-125"
-                    >
-                      {message.likedBy && message.likedBy.includes(userId) ? (
-                        <AiFillHeart className="text-2xl" />
-                      ) : (
-                        <AiOutlineHeart className="text-2xl" />
-                      )}
-                    </button>
-                    <span className="ml-2 text-lg">{message.likes || 0}</span>
+                  <p className="mb-4 text-lg">{message.text}</p>{" "}
+                  {/* Increased bottom margin for space */}
+                  <div className="mt-auto flex items-end justify-between">
+                    <div className="flex items-center">
+                      <button
+                        onClick={() => likeMessage(message.id)}
+                        className="text-accent transition-transform duration-200 ease-in-out hover:scale-125"
+                      >
+                        {message.likedBy && message.likedBy.includes(userId) ? (
+                          <AiFillHeart className="text-2xl" />
+                        ) : (
+                          <AiOutlineHeart className="text-2xl" />
+                        )}
+                      </button>
+                      <span className="ml-2 text-lg">{message.likes || 0}</span>
+                    </div>
+                    <p className="text-xs text-gray-400">{message.time}</p>
                   </div>
-                  <p className="absolute bottom-4 right-4 text-xs text-gray-400">
-                    {message.time}
-                  </p>
                 </div>
               ))}
           </div>
